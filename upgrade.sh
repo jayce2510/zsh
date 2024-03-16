79eff34f77f07d12446dc56fd1157853439e1f70
jayce2510 <104639552+jayce2510@users.noreply.github.com>
2024-03-17 00:14:29 +0700

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+  
 #Install p10k theme & autosuggestions & zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
- 
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~}/zsh-syntax-highlighting
+
+echo "source ${ZSH_CUSTOM:-~}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZSH_CUSTOM:-~}/.zshrc
 
 source zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
