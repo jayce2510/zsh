b06977948596a1f91a68f07e5ebfd7f3e8af67e8
jayce2510 <alviss2510@gmail.com>
2024-03-17 09:58:00 +0700

    update

--- upgrade.sh
+++ upgrade.sh
@@ -5,16 +5,16 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~}/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/zsh-syntax-highlighting
 
 echo "source ${ZSH_CUSTOM:-~}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZSH_CUSTOM:-~}/.zshrc
 
 source ${ZSH_CUSTOM:-~}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 #Edit ZSH config file
-sed -i 's\ZSH_THEME="robbyrussell"\ZSH_THEME="powerlevel10k/powerlevel10k"\' ${ZSH_CUSTOM:-~}/.zshrc
+sed -i 's\ZSH_THEME="robbyrussell"\ZSH_THEME="powerlevel10k/powerlevel10k"\' ${ZDOTDIR:-$HOME}/.zshrc
 
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ${ZSH_CUSTOM:-~}/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ${ZDOTDIR:-$HOME}/.zshrc
 
 #Restart ZSH and set up
 exec zsh
