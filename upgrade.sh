9618bf5bb7f74adc48abf61e2e0e192bf831d808
jayce2510 <alviss2510@gmail.com>
2024-03-17 11:08:58 +0700

    update

--- upgrade.sh
+++ upgrade.sh
@@ -16,8 +16,6 @@ sed -i 's\ZSH_THEME="robbyrussell"\ZSH_THEME="powerlevel10k/powerlevel10k"\' ${Z
 
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ${ZDOTDIR:-$HOME}/.zshrc
 
-echo "(( ${+ZSH_HIGHLIGHT_STYLES} )) || typeset -A ZSH_HIGHLIGHT_STYLES" >> ${ZDOTDIR:-$HOME}/.zshrc
-
 echo "ZSH_HIGHLIGHT_STYLES[path]=none" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 echo "ZSH_HIGHLIGHT_STYLES[path_prefix]=none" >> ${ZDOTDIR:-$HOME}/.zshrc
